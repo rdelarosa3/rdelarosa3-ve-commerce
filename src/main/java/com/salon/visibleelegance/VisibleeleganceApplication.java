1cd480b727f9f3ff6ba323a263b0ced8afb64c85
Robert De la Rosa <rdelarosa39@gmail.com>
2021-03-19 19:25:31 -0500

    Initial commit

---
+++ src/main/java/com/salon/visibleelegance/VisibleeleganceApplication.java
@@ -0,0 +1,13 @@
+package com.salon.visibleelegance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisibleeleganceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisibleeleganceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salon/visibleelegance/VisibleeleganceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salon.visibleelegance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisibleeleganceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
